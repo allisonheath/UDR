546374b968ae854f4e1bd58e1d4e700151c718e7
mtgreenway <mgreenway@uchicago.edu>
2013-04-20 19:55:54 -0500

    working now local to remote

--- udt/tuner/tune.sh
+++ udt/tuner/tune.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
+# example:
+# ./tune.sh 127.0.0.1 lacadmin /home/lacadmin/udr_crypto/udt/tuner/appserver /home/lacadmin/udr_crypto/udt/src
+
 port=9000
 host=$1
 remote_user=$2
 
 count=0
-interval=30
+interval=60
 settings_file=settings.txt
-sever=$3
+server_command=$3
 client=./appclient
+remote_udt_path=$4
+export_ld="export LD_LIBRARY_PATH=$remote_udt_path"
 
-# assumes the file has serverline\n clientline\n sysctl file
-while read server_sysctl; do
-    ssh $remote_user@$host "sudo sysctl -p $server_sysctl"
+while read line; do
 
-    read client_sysctl
+    server_sysctl=$(echo $line|cut -f1 -d ,)
+    client_sysctl=$(echo $line|cut -f2 -d ,)
+    server_args=$(echo $line|cut -f3 -d ,)
+    client_args=$(echo $line|cut -f4 -d ,)
+
     sudo sysctl -p $client_sysctl
+    ssh $remote_user@$host "sudo sysctl -p $server_sysctl" <&-
 
-    read server_line
-    ssh $remote_user@$host "$server $port $server_line & pid=\$!;sleep $interval;kill \$pid"
+    ssh $remote_user@$host "$export_ld; $server_command $port $server_args & pid=\$!;sleep $interval;kill \$pid" <&- &
 
-    read client_line
-    $client $host $port $client_line > raw_out_test$count &
+    $client $host $port $client_args > raw_out_test$count &
     client_pid=$!
 
     sleep $interval
-
     kill $client_pid
 
     count=$((count+1))
-
 done < $settings_file
